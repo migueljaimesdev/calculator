d60cf13809353e22df258ba01b706c5c4327d42c
Miguel Jaimes <migueljaimes@revolutioontech.com>
2021-04-15 22:47:20 +0200

    Creating app structure

--- IOS-Calculator/AppDelegate.swift
+++ IOS-Calculator/AppDelegate.swift
@@ -13,10 +13,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Setup
+        setupView()
+        
         return true
     }
 
+    // MARK: - Private methods
+    
+    private func setupView() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
 
 }
 
---
+++ IOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  IOS-Calculator
+//
+//  Created by Miguel Alejandro on 15/4/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
